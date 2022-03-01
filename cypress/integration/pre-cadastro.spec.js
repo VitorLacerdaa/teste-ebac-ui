2b3b73cb3f4bbc4d9f800f22a59bd864f4d5ac00
VitorLacerdaa <lacerdadesouza@outlook.com>
2022-03-01 16:26:17 -0300

    ...

--- cypress/integration/pre-cadastro.spec.js
+++ cypress/integration/pre-cadastro.spec.js
@@ -15,7 +15,7 @@ describe('Funcionalidade Pré cadastro', () => {
 
       
         cy.get('#reg_email').type(emailFaker)
-        cy.get('#reg_password').type('@@102355118v')
+        cy.get('#reg_password').type('@@12345678v')
         cy.get(':nth-child(4) > .button').click()
 
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
